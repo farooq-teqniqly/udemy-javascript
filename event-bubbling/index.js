309bb368a3b6bcd92e52229e99a53ff0860d308a
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-10-30 23:11:25 -0700

    Add event bubbling example
    Introduce a new HTML, JS, and CSS example to demonstrate event bubbling. Clicking elements within the main div toggles a background and displays a message indicating which element was clicked.

---
+++ event-bubbling/index.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const messageDiv = document.getElementById("message");
+
+  document.getElementById("main").addEventListener("click", (e) => {
+    e.target.classList.toggle("text-bg");
+
+    const p = document.createElement("p");
+    p.textContent = `You clicked the ${e.target.tagName.toLowerCase()} element.`;
+
+    messageDiv.replaceChildren();
+    messageDiv.appendChild(p);
+  });
+});
